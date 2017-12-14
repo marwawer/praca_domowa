a65c0c81c656b78a885a75c035aeddedc0b00283
marwawer <marwawer@wp.pl>
2017-12-14 22:07:28 +0100

    a

---
+++ aa.c
@@ -0,0 +1,9 @@
+//
+// Created by mariusz on 14.12.17.
+//
+
+System dziesiętny
+
+Podstawą systemu dziesiętnego jest liczba 10, a wszystkie liczby można zapisać za pomocą dziesięciu cyfr: 0,1,2,3,4,5,6,7,8,9. Jednostka każdego następnego rzędu jest dziesięć razy większa od jednostki rzędu poprzedniego. Liczby zapisujemy jako ciągi cyfr, z których każda jest mnożną potęgi liczby 10.
+
+ai−1ai−2…a2a1a0=ai−1⋅10i−1+ai−2⋅10i−2+…+a2⋅102+a1⋅101+a0⋅100.
